8c78c6e57002e601a7ff4de29e3fdfed9f23c264
cadenceeo <ceohl@mail.fhsu.edu>
2022-10-09 11:44:52 -0500

    new1

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-const canvas = document.querySelector("canvas");
-const context = canvas.getContext("2d")
+let bookshelfDiv = document.getElementById('bookshelf');
 
-// PARAMETERS ARE STARTX_COORD, STARTY_COORD, WIDTH, HEIGHT
-context.fillRect(10, 10, 300, 100)
-context.fillStyle = "blue"
-context.fillRect(10, 10, 300, 100)
+
+
+
+
+
+
+
+
