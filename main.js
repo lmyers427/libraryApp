2de723e5741712e19375a0c1b606323ac6bab75f
cadenceeo <ceohl@mail.fhsu.edu>
2022-10-09 11:44:35 -0500

    new

---
+++ main.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d")
+
+// PARAMETERS ARE STARTX_COORD, STARTY_COORD, WIDTH, HEIGHT
+context.fillRect(10, 10, 300, 100)
+context.fillStyle = "blue"
+context.fillRect(10, 10, 300, 100)
