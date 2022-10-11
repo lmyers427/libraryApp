44bf5867b1ba0f6aac66585c3dc72dcb1feb37c5
Lauren Myers <lmmyers2@mail.fhsu.edu>
2022-10-11 16:59:01 -0600

    updated controllers registerController file and deleted styles folder

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -3,13 +3,13 @@ const bcrypt = require('bcrypt');
 
 const NewUser = async (req, res) => {
 
-    const {user, pwd} = req.body; //may change depending on HTML
-    if(!user || !pwd) return res.status(400).json({'message':'Username and password required'});
+    const {uname, pwd} = req.body; //may change depending on HTML
+    if(!uname || !pwd) return res.status(400).json({'message':'Username and password required'});
 
-    const duplicate = await User.findOne({username: user}).exec();
+    const duplicate = await User.findOne({username: uname}).exec();
 
 
-    if(duplicate) return res.redirect(301, /*have to put something here not sure what?*/  ); //redirect to login page
+    if(duplicate) return res.redirect(301, /*have to put something here not sure what?*/); //redirect to login page
 
     try{
 
@@ -17,14 +17,14 @@ const NewUser = async (req, res) => {
 
         //create user
         const result = await User.create({
-            "username": user,
+            "username": uname,
             "password": hashedPwd
 
         });
 
         console.log(result); //logs result to console
 
-        res.status(201).json({'success': `New user ${user} created!`});
+        res.status(201).json({'success': `New user ${uname} created!`});
 
     }catch(error){
 
