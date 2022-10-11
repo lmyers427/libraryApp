0bf352dd092a17868cb2007f900abe9797b3d48e
Lauren Myers <lmmyers2@mail.fhsu.edu>
2022-10-11 13:16:00 -0600

    Added a route folder

--- server.js
+++ server.js
@@ -11,13 +11,26 @@ const PORT = process.env.PORT || 3500;
 // connect to mongo database  
 connectDB();
 
+//serve static files for css designs
 app.use('/', express.static(path.join(__dirname, '/styles')));
 
-app.get('/' , (req, res) => {
 
-       res.sendFile(path.join(__dirname, 'views', 'index.html'));
+//routes
+app.use('/', require('./routes/root'));
+app.use('/home', require('./routes/home'));
 
+
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
 });
+
 
 //Listen only if MongoDB connects
 mongoose.connection.once('open', () => {
