f185e37af99e7f7169920aaaae731e40aa1223a3
Lauren Myers <lmmyers2@mail.fhsu.edu>
2022-10-11 09:50:18 -0600

    Added nodemon as a dev dependency

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+require('dotenv').config();
 const express = require('express'); 
 const path = require('path');
 const cors = require('cors');
 const app = require('');
 const mongoose = require('mongoose');
 const connectDB = require('./config/dbConn');
-const port = process.env.PORT || 3500; 
+const PORT = process.env.PORT || 3500; 
+
 
 // connect to mongo database  
 connectDB();
+
+//Listen only if MongoDB connects
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    //might need to adjust this depending on how server is built
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
