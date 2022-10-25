b5b4f8c6436043392ff23f90a38b1f4c7ab24fb0
Lauren Myers <lmmyers2@mail.fhsu.edu>
2022-10-25 13:34:38 -0600

    Fixed issue with logout route

--- routes/logout.js
+++ routes/logout.js
@@ -5,7 +5,7 @@ const logoutController = require('../controllers/logoutController');
 
 router.get('/', (req, res) => {
     let message = req.session.message;
-    res.render(path.join(__dirname, '..', 'views', 'logout')); //updated for ejs
+    res.render(path.join(__dirname, '..', 'views', 'logout'), {message: message}); //updated for ejs
 });
 
 router.post('/', logoutController.logOut);
