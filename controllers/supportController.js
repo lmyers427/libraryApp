fde4d23c5fbca2f792b18059ed6949e8047a7e36
Lauren Myers <lmmyers2@mail.fhsu.edu>
2022-11-16 10:28:00 -0700

    Fixed support controller and support page

--- controllers/supportController.js
+++ controllers/supportController.js
@@ -1,24 +1,29 @@
 const Support = require('../model/Support');
+
+
 const NewTicket = async (req, res) => {
 
-    const {recomendation, technicalDifficulty} = req.body; //may change depending on HTML
-    if(!recomendation || !technicalDifficulty) return res.status(400).json({'message':'Please enter a recommendation or Technical Difficulty'});
+    const {recommend, tech} = req.body; //may change depending on HTML
+    
+    //console.log(req.body);
+    
+    if(!recommend || !tech) return res.status(400).json({'message':'Please enter a recommendation or Technical Difficulty'});
 
-    const duplicate = await Support.findOne({recomendation: recomendation}).exec();
+    const duplicate = await Support.findOne({recomendation: recommend}).exec();
 
     if(duplicate) return res.render('../views/home.ejs', { message: req.session.message = 'Book already Recomended' });
 
     try{
 
-        const newTicket = new Ticket();
-        newTicket.recomendation = recomendation;
-        newTicket.technicalDifficulty = technicalDifficulty;
+        const newTicket = new Support();
+        newTicket.Recommendation = recommend;
+        newTicket.TechnicalDifficulty = tech;
         const result = await newTicket.save();
 
-        res.render('../views/home.ejs', {message: req.session.message = 'Ticket successfully created'});
+        res.render('../views/home.ejs', {user: req.session.user, message: req.session.message = 'Ticket successfully created'});
     
     }catch(error){
-        res.status(500).render('../views/home.ejs', {message: req.session.message = 'Failed to create ticket'});
+        res.status(500).render('../views/home.ejs', {user: req.session.user, message: req.session.message = 'Failed to create ticket'});
     }
 }
 
--- routes/support.js
+++ routes/support.js
@@ -4,6 +4,11 @@ const path = require('path');
 const supportController = require('../controllers/supportController');
 
 router.get('/', (req, res) => {
+
+     //checks if session user has been assigned or if user has logged in. If no, routes user back to login page to login first
+     if(!req.session.user) return res.render(path.join(__dirname, '..', 'views', 'login'), {message: "Please Login Before Submitting a Ticket"} );
+
+
     res.render(path.join(__dirname, '..', 'views', 'support')); //with ejs updated to render
     
 });
