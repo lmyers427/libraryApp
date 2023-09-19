5fb357f35020a2951972a35605b26b6af22ecbfa
Lauren Myers <lmmyers2@mail.fhsu.edu>
2023-09-19 13:34:38 -0600

    Updated BackgroundImage and created new Repository

--- config/dbConn.js
+++ config/dbConn.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         // connect to mongoDB via DATABASE URI in .env file (local only)
+        mongoose.set('strictQuery', false);
         await mongoose.connect(process.env.DATABASE_URI, {
             useUnifiedTopology: true,
             useNewUrlParser: true
